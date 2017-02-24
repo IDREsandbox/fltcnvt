08c4948d2f7e58086d303dcc686de0a7dcf8d407
Scott Friedman <scttfrdmn@gmail.com>
2017-02-24 10:50:06 -0800

    Added LICENSE Added Copyright and cleaned up debugging output

--- fltcnvt.py
+++ fltcnvt.py
@@ -1,3 +1,17 @@
+#
+# This software is Copyright © 2017 Regents of the University of California.
+# All Rights Reserved.
+#
+# Created by Scott Friedman (friedman@idre.ucla.edu)
+# UCLA Office of Information Technology
+# Institute for Digital Research and Education
+#
+# Permission to use, copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice, this paragraph and the following three paragraphs appear in all copies.
+#
+# This software program and documentation are copyrighted by The Regents of the University of California. The software program and documentation are supplied "as is", without any accompanying services from The Regents. The Regents does not warrant that the operation of the program will be uninterrupted or error-free. The end-user understands that the program was developed for research purposes and is advised not to rely exclusively on the program for any reason.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+#
 
 import sys
 import os
@@ -20,13 +34,12 @@ def main ():
     mgExit()
 
 def processDb( dbFilename, srcDrive, tgtDrive ):
-    print "opening:", dbFilename
+    #print "opening:", dbFilename
     db = mgOpenDb( dbFilename )
     if db == None:
         msgbuf = mgGetLastError()
         print msgbuf, "\n"
-        mgExit()
-        exit()
+        return
 
     gotOne, texIndex, texFilename = mgGetFirstTexture( db )
     while gotOne == MG_TRUE:
@@ -52,7 +65,7 @@ def processDb( dbFilename, srcDrive, tgtDrive ):
                     copyfile( srcTexFilename+".attr", tgtTexFilename+".attr" )
                     print "copied", srcTexFilename, " --> ", tgtTexFilename
 
-        print "mgReplaceTexture:", texIndex, texFilename
+        #print "mgReplaceTexture:", texIndex, texFilename
         mgReplaceTexture( db, texIndex, tgtTexFilename )
         gotOne, texIndex, texFilename = mgGetNextTexture( db )
 
@@ -64,7 +77,7 @@ def processDb( dbFilename, srcDrive, tgtDrive ):
             os.makedirs( tgtDirectory )
 
         copyfile( dbFilename, tgtDbFilename )
-        print "copied", dbFilename, " --> ", tgtDbFilename
+        #print "copied", dbFilename, " --> ", tgtDbFilename
 
     mgSaveAsDb( db, tgtDbFilename )
     #mgWriteDb( db )
@@ -78,12 +91,12 @@ def walk( rec, srcDrive, tgtDrive ):
                 switchRec = mgNewRec( fltSwitch )
                 mgAppend( rec, switchRec )
                 items = comment.split( )[1:]
-                print "SWITCH: ", items
+                #print "SWITCH: ", items
                 for item in items:
                     xrefRec = mgNewRec( fltXref )
                     mgSetAttList( xrefRec, fltXrefFilename, item )
                     mgAppend( switchRec, xrefRec )
-                    print "adding xref:", item
+                    #print "adding xref:", item
 
                 #mgSetSwitchBit( switchRec, 0, 0, MG_TRUE )
                 #print "switch mask/bit:", 0, "/", 0
@@ -92,7 +105,7 @@ def walk( rec, srcDrive, tgtDrive ):
                         mgAddSwitchMask( switchRec )
 
                     mgSetSwitchBit( switchRec, idx, idx, MG_TRUE )
-                    print "switch mask/bit:", idx, "/", idx
+                    #print "switch mask/bit:", idx, "/", idx
 
         code = mgGetCode( rec )
         if code == fltHeader:
@@ -119,7 +132,7 @@ def walk( rec, srcDrive, tgtDrive ):
             tgtXrefFilename = re.sub( srcDrive, tgtDrive, srcXrefFilename )
             mgSetAttList( rec, fltXrefFilename, tgtXrefFilename )
             if not os.path.exists( tgtXrefFilename ):
-                print "xref processDb", srcXrefFilename
+                #print "xref processDb", srcXrefFilename
                 processDb( srcXrefFilename, srcDrive, tgtDrive )
             else:
                 #print tgtXrefFilename, " exists"
